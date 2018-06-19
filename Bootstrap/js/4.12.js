18088346d39f87a8fb4e8a60c1c598823ba4b798
Izan Calderaro <izancalderaro@yahoo.com.br>
2018-06-19 18:51:23 -0400

    4.13 validação de formularo jquery e classes de feedback

---
+++ Bootstrap/js/4.12.js
@@ -0,0 +1,23 @@
+$(function () {
+  $('.needs-validation').on('submit', function (event) {
+    if (event.currentTarget.checkValidity() === false) { // mesmo que 'this'
+      event.preventDefault();
+      event.stopPropagation();
+      $('.needs-validation').addClass('was-validated');
+    }
+  });
+
+
+  $('#btnOS').on('click', function () {
+
+    // alert('teste');
+
+    $('.needs-validation').each(function () {      
+      this.reset(); 
+    });
+    
+    $('.needs-validation').removeClass('was-validated');
+
+  });
+
+});
