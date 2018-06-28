e400a0c6e05dff6ed4da4803d4fb9e808baccb2e
Izan Calderaro <izancalderaro@yahoo.com.br>
2018-06-28 18:21:52 -0400

    teste terminado

--- Bootstrap/js/teste.js
+++ Bootstrap/js/teste.js
@@ -4,45 +4,50 @@ $(function () {
 
     var CheckboxDentroDoTbody = $('tbody :input[type=checkbox]');
 
+    $('#todos').on('change', function () {
+        //alert('change');
+    })
+
     $('#todos').on('click', function () {
 
-        // if (this.checked) {
-
-        //     $('tbody :input[type=checkbox]').prop('checked', true);
-
-        // } else {
-
-        //     $('tbody :input[type=checkbox]').prop('checked', false);
-        // }
-
         CheckboxDentroDoTbody.prop('checked', this.checked);
-        
-        $('tbody tr').toggleClass('selecionado');
-
+        marcarTodasAsLinhas();
+        // alert('clicado');
     });
-
 
     CheckboxDentroDoTbody.on('click', function () {
 
-        //var total = $('tbody :input[type=checkbox]:checked').length;
-        var total = CheckboxDentroDoTbody.filter(':checked').length;
+        //var total = CheckboxDentroDoTbody.prop('checked').length; //total indefinido
+        //var total = this.checked.length;  //total indefinido
+        //var total = $('tbody :input[type=checkbox]:checked').length; //total definido
+        var total = CheckboxDentroDoTbody.filter(':checked').length; // filtra os elementos já retornados na opção acima ele vai novamente no DOM buscar os elementos
         var checar = CheckboxDentroDoTbody.length === total;
-
         $('#todos').prop('checked', checar);
-
         console.log('total', total);
-
     });
 
-
     CheckboxDentroDoTbody.on('change', function (evento) {
-
         var elemento = $(evento.target);
-        
         elemento.parents('tr').toggleClass('selecionado');
-        
+        //marcarTodasAsLinhas();
         console.log('evento', elemento);
     });
 
+    function marcarTodasAsLinhas() {
+        // if ($('#todos').is(':checked')) {
+        //     $('tbody tr').addClass('selecionado');
+        // }
+        // else {
+        //     $('tbody tr').removeClass('selecionado');
+        //}
+
+        //ou assim
+       
+        CheckboxDentroDoTbody.filter(':checked').parents('tr').addClass('selecionado');
+        CheckboxDentroDoTbody.filter(':not(:checked)').parents('tr').removeClass('selecionado');
+    
+    }    
+
 });
+
 
